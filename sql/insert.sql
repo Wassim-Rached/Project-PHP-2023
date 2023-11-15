595b5977d0f2e4b511eeaff6a0ca6687ce39ebea
Wassim-Rached <wassimrached404@gmail.com>
2023-11-15 21:42:43 +0100

    Add multiple insert statements for Etudiant and Enseignant tables

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,6 +1,54 @@
-INSERT INTO Etudiant (NCE, nom, prenom, classe) 
-	VALUES
-		('123', 'Riahi', 'Ahmed', 'DSI31'),
-		('456', 'Ben Salem', 'Serine', 'DSI22'),
-		('789', 'Gharbi', 'Faten', 'RSI21'), 
-		('963', 'Ben Amor', 'Tarek', 'TI101');
+INSERT INTO Etudiant (
+	NCE,
+	nom,
+	prenom,
+	classe
+) VALUES (
+	'123',
+	'Riahi',
+	'Ahmed',
+	'DSI31'
+),
+(
+	'456',
+	'Ben Salem',
+	'Serine',
+	'DSI22'
+),
+(
+	'789',
+	'Gharbi',
+	'Faten',
+	'RSI21'
+),
+(
+	'963',
+	'Ben Amor',
+	'Tarek',
+	'TI101'
+);
+
+INSERT INTO Enseignant (
+	Matricule,
+	nom_Ens,
+	prenom_Ens
+) VALUES (
+	'0001',
+	'Ben Ali',
+	'Amira'
+),
+(
+	'0002',
+	'Khelifi',
+	'Youssef'
+),
+(
+	'0003',
+	'Jendoubi',
+	'Noura'
+),
+(
+	'0004',
+	'Bouazizi',
+	'Mehdi'
+);
