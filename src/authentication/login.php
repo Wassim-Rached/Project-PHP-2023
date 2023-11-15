717ee29d7e4ff14622f905fd025e2885ab21b1a6
Wassim-Rached <wassimrached404@gmail.com>
2023-11-15 23:10:40 +0100

    finish login

--- src/authentication/login.php
+++ src/authentication/login.php
@@ -1,25 +1,50 @@
 <?php
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-	// Handle form submission
-	$username = $_POST['username'];
-	$password = $_POST['password'];
+    include('utils/connexion.php');
+    
+    // Handle form submission
+    $login = $_POST['login'];
+    $mot_de_passe = $_POST['mot_de_passe'];
 
-	// TODO: Validate username and password
+    // Use prepared statement to prevent SQL injection
+    $query = 'SELECT * FROM Administrateur WHERE login = :login AND mot_de_passe = :mot_de_passe';
+    $statement = $cnx->prepare($query);
 
-	// TODO: Authenticate user
+    // Bind parameters
+    $statement->bindParam(':login', $login);
+    $statement->bindParam(':mot_de_passe', $mot_de_passe);
 
-	// TODO: Redirect to dashboard or show error message
+    // Execute the prepared statement
+    $statement->execute();
+
+    // Fetch results
+	$row = $statement->fetch();
+
+	// check if the user exists
+	if ($row) {
+		// start the session
+		session_start();
+
+		// store data in SESSION variables
+		$_SESSION['login'] = $row['login'];
+
+		// redirect to the home page
+		header('Location: index.php');
+	} else {
+		// display error message
+		echo 'Login failed';
+	}
 }
 
 ?>
 
 <form method="POST">
-	<label for="username">Username:</label>
-	<input type="text" id="username" name="username" required>
+    <label for="login">login:</label>
+    <input type="text" id="login" name="login" required>
 
-	<label for="password">Password:</label>
-	<input type="password" id="password" name="password" required>
+    <label for="mot_de_passe">mot_de_passe:</label>
+    <input type="password" id="mot_de_passe" name="mot_de_passe" required>
 
-	<button type="submit">Login</button>
+    <button type="submit">Login</button>
 </form>
