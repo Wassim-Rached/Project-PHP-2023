de1c63bc8491b37cb89047b174a3ac0bf96a05c0
Wassim-Rached <wassimrached404@gmail.com>
2023-11-15 21:12:39 +0100

    Add SQL insert statements for Etudiant table

---
+++ sql/insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO Etudiant (NCE, nom, prenom, classe) 
+	VALUES
+		('123', 'Riahi', 'Ahmed', 'DSI31'),
+		('456', 'Ben Salem', 'Serine', 'DSI22'),
+		('789', 'Gharbi', 'Faten', 'RSI21'), 
+		('963', 'Ben Amor', 'Tarek', 'TI101');
