693ee34456e69edcbe09eb726ba023ef014b2483
Wassim-Rached <wassimrached404@gmail.com>
2023-11-15 23:10:49 +0100

    finish logout

--- src/authentication/logout.php
+++ src/authentication/logout.php
@@ -1,5 +1,20 @@
 <?php
 
-	echo "logout";
+	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+		session_start();
 
+		if (isset($_SESSION['login'])) {
+			$_SESSION = array();
+
+			session_destroy();
+
+			header('Location: index.php');
+		} else {
+			header("Location: index.php");
+		}
+	}
 ?>
+
+<form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
+	<button type="submit">Logout</button>
+</form>
