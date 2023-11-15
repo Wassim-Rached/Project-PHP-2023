8fd1d0a0858275742ca7f9bb08b0d0bbcc94bb8c
Wassim-Rached <wassimrached404@gmail.com>
2023-11-16 00:55:16 +0100

    Refactor login.php to prevent SQL injection

--- src/authentication/login.php
+++ src/authentication/login.php
@@ -3,43 +3,32 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     include('utils/connexion.php');
     
-    // Handle form submission
     $login = $_POST['login'];
     $mot_de_passe = $_POST['mot_de_passe'];
 
-    // Use prepared statement to prevent SQL injection
     $query = 'SELECT * FROM Administrateur WHERE login = :login AND mot_de_passe = :mot_de_passe';
     $statement = $cnx->prepare($query);
 
-    // Bind parameters
     $statement->bindParam(':login', $login);
     $statement->bindParam(':mot_de_passe', $mot_de_passe);
-
-    // Execute the prepared statement
     $statement->execute();
 
-    // Fetch results
 	$row = $statement->fetch();
 
-	// check if the user exists
 	if ($row) {
-		// start the session
 		session_start();
 
-		// store data in SESSION variables
 		$_SESSION['login'] = $row['login'];
 
-		// redirect to the home page
 		header('Location: index.php');
 	} else {
-		// display error message
 		echo 'Login failed';
 	}
 }
 
 ?>
 
-<form method="POST">
+<form method="POST" action="<?php $_SERVER['PHP_SELF'] ?>">
     <label for="login">login:</label>
     <input type="text" id="login" name="login" required>
 
