fe73ab4944f24edfd5a5766bb1f122ab051ee1f2
Wassim-Rached <wassimrached404@gmail.com>
2023-12-01 17:02:08 +0100

    work on etuduiant

--- src/etudiant/ajouter_etudiant.php
+++ src/etudiant/ajouter_etudiant.php
@@ -1,48 +1,63 @@
 <?php
 
-	include('../utils/adminOnlyPage.php');
+include('../utils/adminOnlyPage.php');
 
-	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-		
-		include('../utils/connexion.php');
-		$NCE = $_POST['NCE'];
-		$nom = $_POST['nom'];
-		$prenom = $_POST['prenom'];
-		$classe = $_POST['classe'];
+include('../utils/connexion.php');
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+	$NCE = $_POST['NCE'];
+	$nom = $_POST['nom'];
+	$prenom = $_POST['prenom'];
+	$classe = $_POST['classe'];
+
+	try {
 		$mutation = "INSERT INTO Etudiant (NCE, nom, prenom, classe) VALUES (?, ?, ?, ?)";
 		$statement = $cnx->prepare($mutation);
 		$statement->bindParam(1, $NCE);
 		$statement->bindParam(2, $nom);
 		$statement->bindParam(3, $prenom);
 		$statement->bindParam(4, $classe);
-	
+
 		$success = $statement->execute();
-		
-		if ($success) {
-			echo "Etudiant ajouté avec succès";
-		}else{
-			echo "Erreur lors de l'ajout de l'étudiant";
-		}
+	} catch (\Throwable $th) {
+		//throw $th;
 	}
+}
 ?>
 
-<form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
-	<div>
-		<label for="NCE">NCE</label>
-		<input type="number" name='NCE'/>
-	</div>
-	<div>
-		<label for="nom">Nom</label>
-		<input type="text" name='nom'/>
-	</div>
-	<div>
-		<label for="prenom">Prenom</label>
-		<input type="text" name='prenom'/>
-	</div>
-	<div>
-		<label for="classe">classe</label>
-		<input type="text" name='classe'/>
-	</div>
-    <button type="submit">create</button>
-</form>
+<div class="container">
+	<h1>
+		Ajouter un étudiant
+	</h1>
+	<?php if (isset($success)) : ?>
+		<?php if ($success) : ?>
+			<div class='alert alert-success'>Etudiant modifié avec succès</div>
+		<?php else : ?>
+			<div class='alert alert-danger'>Aucune modification n'a été apportée</div>
+		<?php endif ?>
+	<?php endif ?>
+	<form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
+		<div>
+			<label class="form-label" for="NCE">NCE</label>
+			<input class="form-control" type="number" name='NCE' />
+		</div>
+		<div>
+			<label class="form-label" for="nom">Nom</label>
+			<input class="form-control" type="text" name='nom' />
+		</div>
+		<div>
+			<label class="form-label" for="prenom">Prenom</label>
+			<input class="form-control" type="text" name='prenom' />
+		</div>
+		<div>
+			<label class="form-label" for="classe">classe</label>
+			<input class="form-control" type="text" name='classe' />
+		</div>
+		<button type="submit" class='btn btn-primary mt-3'>create</button>
+	</form>
+</div>
+
+
+<link rel="stylesheet" href="/css/navbar.css">
+<script src="/js/navbar.js"></script>
