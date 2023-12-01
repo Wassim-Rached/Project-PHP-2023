713a7203d98ccee3a4f6f2b1f3832d7e7b5a7026
Wassim-Rached <wassimrached404@gmail.com>
2023-12-01 17:02:32 +0100

    add styling

--- src/utils/connexion.php
+++ src/utils/connexion.php
@@ -1,14 +1,12 @@
 <?php
 
-	include("dbconfig.php");
-	
-	try {
-		$cnx = new PDO('mysql:host='.$host.':'.$port.';dbname='.$dbname,$username,$password);
-		echo '<p>connexion réussie</p>';
-	} catch (PDOException $e) {
-		echo "error : ".$e->getMessage();
-		echo "code : ".$e->getCode();
-	}
+include("dbconfig.php");
 
-
-?>
+try {
+	$cnx = new PDO('mysql:host=' . $host . ':' . $port . ';dbname=' . $dbname, $username, $password);
+	echo '<p class="badge bg-success m-2">connexion réussie</p>';
+} catch (PDOException $e) {
+	echo '<p class="badge bg-danger m-2">erreur de connexion</p>';
+	// echo "error : " . $e->getMessage();
+	// echo "code : " . $e->getCode();
+}
