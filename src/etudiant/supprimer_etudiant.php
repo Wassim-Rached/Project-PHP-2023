acddc00d5f00fb9863481ce31929eba9bf6d7645
Wassim-Rached <wassimrached404@gmail.com>
2023-12-01 17:02:18 +0100

    update css

--- src/etudiant/supprimer_etudiant.php
+++ src/etudiant/supprimer_etudiant.php
@@ -1,39 +1,43 @@
 <?php
 
-	include('../utils/adminOnlyPage.php');
+include('../utils/adminOnlyPage.php');
 
-	if (!isset($_GET['NCE'])) {
-		exit('NCE is required');
-	}
+if (!isset($_GET['NCE'])) {
+	exit('NCE is required');
+}
 
-	include('../utils/connexion.php');
+include('../utils/connexion.php');
 
-	$NCE = $_GET['NCE'];
+$NCE = $_GET['NCE'];
 
-	$query = 'SELECT * FROM Etudiant WHERE NCE = ' . $NCE;
-	$req = $cnx->query($query);
-	$etudiant = $req->fetch();
+$query = 'SELECT * FROM Etudiant WHERE NCE = ' . $NCE;
+$req = $cnx->query($query);
+$etudiant = $req->fetch();
 
-	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-		$mutation = "DELETE FROM Etudiant WHERE NCE = :NCE";
-		$statement = $cnx->prepare($mutation);
-		$statement->bindParam(':NCE', $NCE);
-	
-		$statement->execute();
-	
-		header('Location: ./liste_etudiants.php');
-		exit('etudiant supprimé');
-	}
+	$mutation = "DELETE FROM Etudiant WHERE NCE = :NCE";
+	$statement = $cnx->prepare($mutation);
+	$statement->bindParam(':NCE', $NCE);
+
+	$statement->execute();
+
+	header('Location: ./liste_etudiants.php');
+	exit('etudiant supprimé');
+}
 
 ?>
 
 <ul>
-	<li><?php echo 'NCE :'. $etudiant['NCE']; ?></li>
-	<li><?php echo 'nom :'. $etudiant['nom']; ?></li>
-	<li><?php echo 'prenom :'. $etudiant['prenom']; ?></li>
-	<li><?php echo 'classe :'. $etudiant['classe']; ?></li>
+	<li><?php echo 'NCE :' . $etudiant['NCE']; ?></li>
+	<li><?php echo 'nom :' . $etudiant['nom']; ?></li>
+	<li><?php echo 'prenom :' . $etudiant['prenom']; ?></li>
+	<li><?php echo 'classe :' . $etudiant['classe']; ?></li>
 </ul>
 <form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
 	<button type="submit">supprimer</button>
 </form>
+
+
+<link rel="stylesheet" href="/css/navbar.css">
+<script src="/js/navbar.js"></script>
